f260eb8198d0ac3279ab6198a1a830aa53b6d34c
Oganesyan Artur <arturris@mail.ru>
2018-12-05 22:35:09 -0600

    Deployemnt

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 import './App.css';
 import logo from './logo.png';
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
+  // uri: "http://localhost:5000/graphql"
 })
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img src={logo}
-            alt="SpaceX"
-            style={{ width: 300, display: 'block', margin: 'auto' }} />
-          <Launches />
-        </div>
+        <BrowserRouter>
+          <div className="container">
+            <img src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: 'block', margin: 'auto' }} />
+            <Route exact path='/' component={Launches} />
+            <Route path='/launch:flight_number' component={Launch} />
+          </div>
+        </BrowserRouter>
       </ApolloProvider>
     );
   }
---
+++ public/precache-manifest.b12297615e7237fe227ea6f6d90be1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03cfb11513c4e3ff3781",
+    "url": "/static/js/main.03cfb115.chunk.js"
+  },
+  {
+    "revision": "80692208c958c4ccb2c8",
+    "url": "/static/js/1.80692208.chunk.js"
+  },
+  {
+    "revision": "03cfb11513c4e3ff3781",
+    "url": "/static/css/main.1f6703c9.chunk.css"
+  },
+  {
+    "revision": "68a209c57aa1cb975d2435dd4b8c70fc",
+    "url": "/index.html"
+  }
+];
